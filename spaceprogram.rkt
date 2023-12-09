13cf840d2a8dba1ea0d60dd29d6ae26c86bab654
Robert Lutz <robertlutzi@gmail.com>
2023-12-09 16:03:58 -0500

    streamline vector math

--- spaceprogram.rkt
+++ spaceprogram.rkt
@@ -40,75 +40,90 @@
 
 ; functions
 
-; Constellation -> Constellation
-; run the pocket universe
 (define (main stelln)
+  ; Constellation -> Constellation
+  ; run the pocket universe
   (big-bang stelln
     [to-draw render]
     [on-tick update-constellation]
     ; [on-key impulse)) !!!
     ))
 
-; Vector -> Vector
-; a function that updates the acceleration vector based on the current position
-(check-within (update-acceleration (make-vector (/ WIDTH 2) (+ (/ HEIGHT 2) 12))
-                                   ORIGIN)
-              (make-vector 0 (* (/ GRAVITY (expt (normalize (make-vector (/ WIDTH 2) (+ (/ HEIGHT 2) 12)) ORIGIN) 3)) -12)) 1/1000000)
-(check-within (update-acceleration (make-vector (- (/ WIDTH 2) 5) (/ HEIGHT 2))
-                                   ORIGIN)
-              (make-vector (* (/ GRAVITY (expt (normalize (make-vector (- (/ WIDTH 2) 5) (/ HEIGHT 2)) ORIGIN) 3)) 5) 0) 1/1000000)
-(check-within (update-acceleration (make-vector (- (/ WIDTH 2) 5) (+ (/ HEIGHT 2) 12))
-                                   ORIGIN)
-              (make-vector (* (/ GRAVITY (expt (normalize
-                                                (make-vector (- (/ WIDTH 2) 5) (+ (/ HEIGHT 2) 12))
-                                                ORIGIN) 3)) 5)
-                           (* (/ GRAVITY (expt (normalize
-                                                (make-vector (- (/ WIDTH 2) 5) (+ (/ HEIGHT 2) 12))
-                                                ORIGIN) 3)) -12)) 1/1000000)
 (define (update-acceleration p p0)
-  (c*vec (/ GRAVITY (expt (normalize p p0) 3)) (-vec p0 p)))
+  ; Vector -> Vector
+  ; a function that updates the acceleration vector based on the current position
+  (c*vec GRAVITY (inverse-sqr (-vec p0 p))))
+; checks
+(check-within (update-acceleration (make-vector 10 0) (make-vector 0 0)) (c*vec GRAVITY (make-vector -1/100 0)) 1e-10)
+(check-within (update-acceleration (make-vector 0 5) (make-vector 0 -5)) (c*vec GRAVITY (make-vector 0 -1/100)) 1e-10)
+(check-within (update-acceleration (make-vector -3 12) (make-vector 2 0)) (c*vec GRAVITY (make-vector 5/2197 -12/2197)) 1e-10)
 
-; Vector, Vector -> Number
-; normalizes the distance between two vectors in cartesian space
-(check-within (normalize (make-vector 12 5) (make-vector 12 5)) 0 1/1000000)
-(check-within (normalize (make-vector 12 5) (make-vector 0 0)) 13 1/1000000)
-(check-within (normalize (make-vector 0 0) (make-vector 12 5)) 13 1/1000000)
-(define (normalize p1 p2)
-  (sqrt (+ (sqr (- (vector-x p1) (vector-x p2)))
-           (sqr (- (vector-y p1) (vector-y p2))) 1.3e-134))) ; there's some kind of crazy rounding error going on here that sometimes causes the program to virtually halt
+(define (normalize vec)
+  ; Vector -> Number
+  ; normalizes the length of a vector in cartesian space
+  (sqrt (+ (expt (vector-x vec) 2)
+           (expt (vector-y vec) 2)
+           1e-120) ; some bug in finding the roots of  numbers?
+        ))
+; checks
+(check-within (normalize (make-vector 0 0)) 0 1e-10)
+(check-within (normalize (make-vector 12 5)) 13 1e-10)
+(check-within (normalize (make-vector -12 5)) 13 1e-10)
 
-; Vector, Vector -> Vector
-; add one vector to another
+(define (inverse-sqr vec)
+  ; Vector -> Vector
+  ; calculates the inverse square of a vector, useful for physics
+  (c*vec (/ 1 (sqrt (+ (* 1 (expt (vector-x vec) 6) (expt (vector-y vec) 0))
+                       (* 3 (expt (vector-x vec) 4) (expt (vector-y vec) 2))
+                       (* 3 (expt (vector-x vec) 2) (expt (vector-y vec) 4))
+                       (* 1 (expt (vector-x vec) 0) (expt (vector-y vec) 6))
+                       1e-120) ; some bug in finding the roots of  numbers?
+                    )) vec))
+; checks
+(check-within (inverse-sqr (make-vector 10 0)) (make-vector 1/100 0) 1e-10) ; checks
+
+(define (+vec v1 v2)
+  ; Vector, Vector -> Vector
+  ; add one vector to another
+  (make-vector (+ (vector-x v1) (vector-x v2))
+               (+ (vector-y v1) (vector-y v2))))
+; checks
 (check-expect (+vec (make-vector 12 5) (make-vector 12 5)) (make-vector 24 10))
 (check-expect (+vec (make-vector 12 5) (make-vector 0 0)) (make-vector 12 5))
 (check-expect (+vec (make-vector 0 0) (make-vector 12 5)) (make-vector 12 5))
-(define (+vec v1 v2)
-  (make-vector (+ (vector-x v1) (vector-x v2))
-               (+ (vector-y v1) (vector-y v2))))
 
-; Vector, Vector -> Vector
-; subtract one vector from another
+(define (-vec v1 v2)
+  ; Vector, Vector -> Vector
+  ; subtract one vector from another
+  (make-vector (- (vector-x v1) (vector-x v2))
+               (- (vector-y v1) (vector-y v2))))
+; checks
 (check-expect (-vec (make-vector 12 5) (make-vector 12 5)) (make-vector 0 0))
 (check-expect (-vec (make-vector 12 5) (make-vector 0 0)) (make-vector 12 5))
 (check-expect (-vec (make-vector 0 0) (make-vector 12 5)) (make-vector -12 -5))
-(define (-vec v1 v2)
-  (make-vector (- (vector-x v1) (vector-x v2))
-               (- (vector-y v1) (vector-y v2))))
 
-; Number, Vector -> Vector
-(check-expect (c*vec 10 (make-vector 10 10)) (make-vector 100 100))
-; multiply a vector by a scalar
 (define (c*vec c vec)
+  ; Number, Vector -> Vector
+  ; multiply a vector by a scalar
   (make-vector (* c (vector-x vec)) (* c (vector-y vec))))
+; checks
+(check-expect (c*vec 10 (make-vector 10 10)) (make-vector 100 100))
 
+(define (update-constellation sats)
 ; Constellation -> Constellation
 ; a function that updates the information for a suite of satellites
-(define (update-constellation sats)
   (make-constellation (update-satellite (constellation-craft sats))
                       (update-satellite (constellation-moon sats))))
 
+(define (update-satellite rkt)
 ; Satellite -> Satellite
 ; a function that updates the rocket information
+  (make-satellite (+vec (satellite-pos rkt) (satellite-vel rkt))
+                  (+vec (satellite-vel rkt) (satellite-acc rkt))
+                  (update-acceleration (satellite-pos rkt) ORIGIN)
+                  (cond [(< (normalize (-vec (satellite-pos rkt) ORIGIN)) EARTHRADIUS) BOOM!]
+                        [else (satellite-image rkt)])))
+; checks
 (check-within (update-satellite (make-satellite
                                  (make-vector 350 300)
                                  (make-vector 0 20)
@@ -118,7 +133,7 @@
                               (make-vector 0 20)
                               (update-acceleration (make-vector 350 300) ORIGIN)
                               SPACECRAFT)
-              1/100000)
+              1e-10)
 (check-within (update-satellite (make-satellite
                                  (+vec ORIGIN (make-vector 10 10))
                                  (make-vector 0 20)
@@ -129,59 +144,50 @@
                               (update-acceleration (+vec ORIGIN (make-vector 10 10))
                                                    ORIGIN)
                               BOOM!)
-              1/100000)
-(define (update-satellite rkt)
-  (make-satellite (+vec (satellite-pos rkt) (satellite-vel rkt))
-                  (+vec (satellite-vel rkt) (satellite-acc rkt))
-                  (update-acceleration (satellite-pos rkt) ORIGIN)
-                  (cond [(< (normalize (satellite-pos rkt) ORIGIN) EARTHRADIUS) BOOM!]
-                        [else (satellite-image rkt)])))
+              1e-10)
 
+(define (render sats)
 ; Constellation -> Img
 ; render an image of the rocket flying around, and the orbiting moon
-(check-expect (render (make-constellation
-                       (make-satellite ORIGIN (make-vector 0 0)
-                                       (make-vector 0 0) SPACECRAFT)
-                       (make-satellite ORIGIN (make-vector 0 0)
-                                       (make-vector 0 0) MOON)))
-              (place-image SPACECRAFT (vector-x ORIGIN) (vector-y ORIGIN)
-                           (place-image MOON (vector-x ORIGIN) (vector-y ORIGIN)
-                                        (place-image EARTH (vector-x ORIGIN)
-                                                     (vector-y ORIGIN) THEVOID)))) ; checks
-(check-expect (render (make-constellation
-                       (make-satellite ORIGIN (make-vector 0 0)
-                                       (make-vector 0 0) SPACECRAFT)
-                       (make-satellite ORIGIN (make-vector 0 0)
-                                       (make-vector 0 0) MOON)))
-              (place-image SPACECRAFT (vector-x ORIGIN) (vector-y ORIGIN)
-                           (place-image MOON (vector-x ORIGIN) (vector-y ORIGIN)
-                                        (place-image EARTH (vector-x ORIGIN)
-                                                     (vector-y ORIGIN) THEVOID)))) ; checks
-(define (render sats)
   (place-image (satellite-image (constellation-craft sats))
                (vector-x (satellite-pos (constellation-craft sats)))
                (vector-y (satellite-pos (constellation-craft sats)))
                (place-image (satellite-image (constellation-moon sats))
                             (vector-x (satellite-pos (constellation-moon sats)))
                             (vector-y (satellite-pos (constellation-moon sats)))
-                            (place-image EARTH (vector-x ORIGIN) (vector-y ORIGIN) THEVOID))))
+                            (place-image EARTH (vector-x ORIGIN) (vector-y ORIGIN)
+                                         THEVOID))))
+;checks
+(check-expect (render (make-constellation
+                       (make-satellite ORIGIN (make-vector 0 0)
+                                       (make-vector 0 0) SPACECRAFT)
+                       (make-satellite ORIGIN (make-vector 0 0)
+                                       (make-vector 0 0) MOON)))
+              (place-image SPACECRAFT (vector-x ORIGIN) (vector-y ORIGIN)
+                           (place-image MOON (vector-x ORIGIN) (vector-y ORIGIN)
+                                        (place-image EARTH (vector-x ORIGIN)
+                                                     (vector-y ORIGIN) THEVOID)))) ; checks
+(check-expect (render (make-constellation
+                       (make-satellite ORIGIN (make-vector 0 0)
+                                       (make-vector 0 0) SPACECRAFT)
+                       (make-satellite ORIGIN (make-vector 0 0)
+                                       (make-vector 0 0) MOON)))
+              (place-image SPACECRAFT (vector-x ORIGIN) (vector-y ORIGIN)
+                           (place-image MOON (vector-x ORIGIN) (vector-y ORIGIN)
+                                        (place-image EARTH (vector-x ORIGIN)
+                                                     (vector-y ORIGIN) THEVOID)))) ; checks
 
+
+(define (impulse rkt ke)
 ; Rocket KeyEvent -> Rocket
 ; allow the user to give an impulse thrust to the rocket
 ; !!!
-(define (impulse rkt ke)
   (cond [(... ke) (... rkt)]
         [(... ke) (... rkt)]))
 
-; Rocket -> Rocket
-; what happens when the rocket crashes back to earth
-; !!!
-(define (crash rkt)
-  rkt)
-
+(define (gravity rkt moon)
 ; Vector, Vector -> Vector
 ; account for gravitational effects of the moon
-(define (gravity rkt moon)
   (satellite-acc rkt))
 
 
