85f691fa0790366d2046cabde1fadb9154ec8bf1
Robert Lutz <robertlutzi@gmail.com>
2023-12-08 23:33:28 -0500

    add a moon

--- spaceprogram.rkt
+++ spaceprogram.rkt
@@ -11,29 +11,25 @@
 ;   make-position [Number Number]
 ; a vector that represents a position in 2D cartesian coordinates
 (define-struct position (x y)) 
-#; (define (fn-with-position p)
-  (... p))
 
 ; A Velocity is a structure
 ;   make-velocity [Number Number]
 ; a vector that represents a velocity in 2D cartesian coordinates
 (define-struct velocity (x y)) 
-#; (define (fn-with-velocity v)
-  (... v))
 
 ; A Acceleration is a structure
 ;   make-acceleration [Number Number]
 ; a vector that represents an acceleration in 2D cartesian coordinates
 (define-struct acceleration (x y)) 
-#; (define (fn-with-acceleration a)
-  (... a))
 
-; A Rocket is a make-rocket [Position Velocity Acceleration]
+; A Satellite is a make-satellite [Position Velocity Acceleration Img]
 ;     a collection of vectors that represent the rocket's position,
 ;          velocity and acceleration in 2D cartesian coordinates
-(define-struct rocket (pos vel acc)) 
-#; (define (fn-with-rocket rkt)
-  (... rkt))
+(define-struct satellite (pos vel acc image)) 
+
+; A Constellation is a make-constellation [Satellite Satellite]
+;     a collection of satellites orbiting earth
+(define-struct constellation (craft moon)) 
 
 
 ; constants
@@ -41,10 +37,12 @@
 (define WIDTH 1400)
 (define HEIGHT 750)
 (define EARTHRADIUS 50)
+(define MOONRADIUS 15)
 (define ORIGIN (make-position (quotient WIDTH 2) (quotient HEIGHT 2)))
 (define GRAVITY 200) ; the force of gravity
 (define THEVOID (empty-scene WIDTH HEIGHT "black"))
 (define EARTH (circle EARTHRADIUS "solid" "light blue"))
+(define MOON (circle MOONRADIUS "solid" "light grey"))
 (define SPACECRAFT (triangle 10 "solid" "silver"))
 
 
@@ -100,40 +98,77 @@
 (check-expect (normalize (make-position 0 0) (make-position 12 5)) 13)
 (define (normalize p1 p2)
   (sqrt (+ (sqr (- (position-x p1) (position-x p2)))
-  (sqr (- (position-y p1) (position-y p2))))))
+           (sqr (- (position-y p1) (position-y p2))))))
+
+; Constellation -> Constellation
+; a function that updates the information for a suite of satellites
+(define (update-constellation sats)
+  (make-constellation (update-satellite (constellation-craft sats))
+                      (update-satellite (constellation-moon sats))))
+
+; Satellite -> Satellite
+; a function that updates the rocket information
+(check-within (update-satellite (make-satellite
+                                 (make-position 350 300)
+                                 (make-velocity 0 20)
+                                 (make-acceleration 0 0)
+                                 SPACECRAFT))
+              (make-satellite (make-position 350 320)
+                              (make-velocity 0 20)
+                              (update-acceleration (make-position 350 300) ORIGIN)
+                              SPACECRAFT)
+              1/100000)
+(define (update-satellite rkt)
+  (make-satellite (update-position (satellite-pos rkt) (satellite-vel rkt))
+                  (update-velocity (satellite-vel rkt) (satellite-acc rkt))
+                  (update-acceleration (satellite-pos rkt) ORIGIN)
+                  (satellite-image rkt)))
+
+; Constellation -> Img
+; render an image of the rocket flying around, and the orbiting moon
+(check-expect (render (make-constellation
+                       (make-satellite ORIGIN (make-velocity 0 0)
+                                       (make-acceleration 0 0) SPACECRAFT)
+                       (make-satellite ORIGIN (make-velocity 0 0)
+                                       (make-acceleration 0 0) MOON)))
+              (place-image SPACECRAFT (position-x ORIGIN) (position-y ORIGIN)
+                           (place-image MOON (position-x ORIGIN) (position-y ORIGIN)
+                                        (place-image EARTH (position-x ORIGIN)
+                                                     (position-y ORIGIN) THEVOID)))) ; checks
+(define (render sats)
+  (place-image (satellite-image (constellation-craft sats))
+               (position-x (satellite-pos (constellation-craft sats)))
+               (position-y (satellite-pos (constellation-craft sats)))
+               (place-image (satellite-image (constellation-moon sats))
+                            (position-x (satellite-pos (constellation-moon sats)))
+                            (position-y (satellite-pos (constellation-moon sats)))
+                            (place-image EARTH (position-x ORIGIN) (position-y ORIGIN) THEVOID))))
+
+; Rocket KeyEvent -> Rocket
+; allow the user to give an impulse thrust to the rocket
+; !!!
+(define (impulse rkt ke)
+  (cond [(... ke) (... rkt)]
+        [(... ke) (... rkt)]))
 
 ; Rocket -> Rocket
-; a function that updates the rocket information
-(check-within (update-rocket (make-rocket
-                              (make-position 350 300)
-                              (make-velocity 0 20)
-                              (make-acceleration 0 0)))
-              (make-rocket (make-position 350 320)
-                           (make-velocity 0 20)
-                           (update-acceleration (make-position 350 300) ORIGIN))
-              1/100000)
-(define (update-rocket rkt)
-  (make-rocket (update-position (rocket-pos rkt) (rocket-vel rkt))
-               (update-velocity (rocket-vel rkt) (rocket-acc rkt))
-               (update-acceleration (rocket-pos rkt) ORIGIN)))
-
-; Rocket -> Img
-; render an image of the rocket flying around
-(check-expect (render (make-rocket ORIGIN (make-velocity 0 0) (make-acceleration 0 0))) (place-image SPACECRAFT (position-x ORIGIN) (position-y ORIGIN) (place-image EARTH (position-x ORIGIN) (position-y ORIGIN) THEVOID))) ; checks
-(define (render rkt)
-  (place-image SPACECRAFT (position-x (rocket-pos rkt)) (position-y (rocket-pos rkt))
-             (place-image EARTH (position-x ORIGIN) (position-y ORIGIN) THEVOID)))
+; what happens when the rocket crashes back to earth
+; !!!
+(define (crash rkt)
+  rkt)
 
 
 ; action!
 
-(define Apollo (make-rocket (make-position (+ (/ WIDTH 2) EARTHRADIUS) (/ HEIGHT 2) )
-                            (make-velocity 0 -1.95)
-                            (make-acceleration 0 0)))
+(define Apollo (make-satellite (make-position (+ (/ WIDTH 2) EARTHRADIUS) (/ HEIGHT 2) )
+                               (make-velocity 0 -1.95)
+                               (make-acceleration 0 0)
+                               SPACECRAFT))
 
-(define Venetia (make-rocket (make-position 0 0)
-                            (make-velocity 0 0)
-                            (make-acceleration 0 0)))
-(big-bang Venetia
+(define Luna (make-satellite (make-position (/ WIDTH 2) (- (/ HEIGHT 2) 350))
+                             (make-velocity -0.75 0)
+                             (make-acceleration 0 0)
+                             MOON))
+(big-bang (make-constellation Apollo Luna)
   [to-draw render]
-  [on-tick update-rocket])
+  [on-tick update-constellation])
